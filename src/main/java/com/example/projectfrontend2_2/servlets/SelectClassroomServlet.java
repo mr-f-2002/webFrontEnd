f6b4e8e1a970559882c3bbc949f2c679faaff2ac
yousu <yousuf.niloy@gmail.com>
2024-05-29 05:02:40 +0600

    Niloy

---
+++ src/main/java/com/example/projectfrontend2_2/servlets/SelectClassroomServlet.java
@@ -0,0 +1,36 @@
+package com.example.projectfrontend2_2.servlets;
+
+import com.example.projectfrontend2_2.DTO.ClassroomDTO;
+import com.example.projectfrontend2_2.DTO.TeacherDTO;
+import com.example.projectfrontend2_2.http.RequestMaker;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "SelectClassroomServlet", value = "/SelectClassroomServlet")
+public class SelectClassroomServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String classIdStr = req.getParameter("classId");
+        if (classIdStr != null) {
+            long classId = Long.parseLong(classIdStr);
+            RequestMaker rqm = new RequestMaker();
+            try {
+                ClassroomDTO classroom = rqm.fetch_classroom(classId);
+                HttpSession session = req.getSession();
+                session.setAttribute("currentClassroom", classroom);
+                resp.sendRedirect("scrolscrene2.jsp");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                resp.sendRedirect("TeacherScene.jsp?error=classFetchFailed");
+            }
+        } else {
+            resp.sendRedirect("TeacherScene.jsp?error=invalidClassId");
+        }
+    }
+}
